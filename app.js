907ead3ed79bc351e21b850afd674f310356ff07
laidOffWorkerLearnsToCode <kwg6qlyv@hotmail.com>
2023-05-10 16:56:18 +0800

    init setup

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const dotenv = require('dotenv')
-const connectAtlas = require('./config/db');
-
+const connectToAtlas = require('./config/db');
+const exphbs = require('express-handlebars');
+const morgan = require('morgan');
+const indexRouter = require('./routes/index');
 const app = express();
+const path = require('path');
+
+// 设置静态文件夹
+app.set('views', path.join(__dirname, 'views'));
+// 设置morgan日志
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// 设置handlebars模板引擎
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: '.hbs'
+});
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+
+// 托管静态资源
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 路由
+app.use('/', indexRouter);
+
 dotenv.config({'path': './config/config.env'})
-connectAtlas();
+
+connectToAtlas();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
--- config/db.js
+++ config/db.js
@@ -1,17 +1,22 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const axios = require('axios');
 dotenv.config({path: './config/config.env'});
 
-async function connectAtlas() {
+const getIp = async () => {
+    const res = await axios.get('https://api.ipify.org?format=json');
+    console.log(`Your IP address is ${res.data.ip}`);
+}
+
+const connectToAtlas = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('MongoDB connected:');
+        await getIp();
+        const connect = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB connected: ${connect.connection.host}`);
     } catch (error) {
-        console.log(error);
+        console.log(`Error: ${error.message}`);
         process.exit(1);
     }
-};
+}
 
-connectAtlas();
-
-module.exports = connectAtlas;
+module.exports = connectToAtlas;
